5208e6e25e34604db10b2d8605f26a570b439853
Themis404 <legen_waitforit_dary@mail.ru>
2018-02-17 19:42:19 +1000

    First task

--- one.py
+++ one.py
@@ -1,1 +1,18 @@
-print("что-то новое")
+s = "Let's go"
+while s != 'stop':
+    a = int(input("Enter А "))
+    b = int(input("Enter В "))
+    operation = input("Select one from -,+,/,* ")
+    def calculator():
+        if operation == '*':
+            print(a*b)
+        elif operation == '/':
+            print(a/b)
+        elif operation == '-':
+            print(a-b)
+        elif operation == '+':
+            print(a+b)
+        else:
+            print("sorry")
+    calculator()
+    s = input("well done, if it's all, then must write 'stop' ")
