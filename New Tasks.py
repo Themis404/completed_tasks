823496ff0258c09be2451273670cac96f9780bd6
Themis404 <legen_waitforit_dary@mail.ru>
2018-02-18 20:53:36 +1000

    Bread tasks

---
+++ New Tasks.py
@@ -0,0 +1,29 @@
+'''
+4.Длина Московской кольцевой автомобильной дороги —109 километров.
+Стартуем с нулевого километра МКАД и едем со скоростью V километров в час.
+На какой отметке остановимся через T часов?
+Программа получает на вход значение V и T.
+Если V>0, то движемся в положительном направлении по МКАД,
+если же значение V<0, то в отрицательном.
+Программа должна вывести целое число от 0 до 108 — номер отметки,
+на которой остановимся.
+'''
+
+road = int(109)
+moment_road = int(0)
+speed = int(input("Enter the speed of your car: "))
+time = int(input("How many hours your car will drive"))
+def stop_road():
+    moment_road = abs(speed * time)
+    while moment_road >= road:
+        moment_road = moment_road - road
+    print(moment_road)
+if speed > 0:
+    print("Your car will go clockwise")
+    stop_road()
+elif speed == 0:
+    print("Your car doesn't go")
+    stop_road()
+else:
+    print("Your car will go counterclockwise")
+    stop_road()
