a68a5ff44713e41bbf68089d3e1d6421acadfdd4
Themis404 <legen_waitforit_dary@mail.ru>
2018-03-11 17:47:45 +1000

    классы (дополнено)

--- task_fourteen/task_fourteen.py
+++ task_fourteen/task_fourteen.py
@@ -6,10 +6,10 @@
 
 class Fraction:
 
-    def __init__(self, numerator, denominator):
+    def __init__(self, numerator, denominator, addend):
         self.numerator = numerator
         self.denominator = denominator
-
+        self.addend = addend
 
 class Operations:
 
@@ -28,16 +28,40 @@ class Operations:
     def multiplication(self):
         return self.fraction_to_operate.numerator * self.fraction_to_operate.denominator
 
+    def sum_num_and_addend(self):
+        self.fraction_to_operate.numerator += self.fraction_to_operate.addend
+        return self.fraction_to_operate.numerator
+
+    def common_divisor(self):
+        num_common_divisor = self.fraction_to_operate.numerator
+        den_common_divisor = self.fraction_to_operate.denominator
+
+        while num_common_divisor != 0 and den_common_divisor != 0:
+            if num_common_divisor > den_common_divisor:
+                num_common_divisor = num_common_divisor % den_common_divisor
+            else:
+                den_common_divisor = den_common_divisor % num_common_divisor
+
+        return den_common_divisor + num_common_divisor
+
+
+
 def main():
 
-    num = int(input("Введите первое значение "))
-    den = int(input("Введите второе значение "))
+    num = int(input("Введите числитель "))
+    den = int(input("Введите знаменатель "))
+    addend = int(input("Число для суммы с числителеем "))
 
-    test = Fraction(num, den)
+    test = Fraction(num, den, addend)
     operTest = Operations(test)
+
+    print("Дробь: ", num, '/', den)
+    print("Сокращение дробей: ", int(num/operTest.common_divisor()), '/', int(den/operTest.common_divisor()))
     print("Из двух чисел больше: ", operTest.get_max())
-    print("Сложение: ", operTest.plus())
+    print("Сложение числителя и знаменателя: ", operTest.plus())
     print("Умножение: ", operTest.multiplication())
+    print("Сложение числителя с новым значением: ", operTest.sum_num_and_addend())
+    print("Сложение числителя и знаменателя: ", operTest.plus())
 
 if __name__ == '__main__':
     main()
